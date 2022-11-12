51e0a029186804e6576d7f3aa6c0ce97ecd98ee1
Igor Patro <i.patro@wp.pl>
2022-11-12 15:20:48 +0100

    Added no albums info

--- src/components/Albums/Albums.tsx
+++ src/components/Albums/Albums.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { useAlbumsStore } from "store/AlbumsStore"
+import { useLangContext } from "context/LangContext"
 import { useFilterSettingsContext } from "context/FilterSettingsContext"
 import { useViewContext } from "context/ViewContext"
 import { filterAlbums } from "utils/filterAlbums"
@@ -7,10 +8,21 @@ import Table from "./Table/Table"
 import Grid from "./Grid/Grid"
 
 const Albums = () => {
+  const { internationalizeMessage } = useLangContext()
   const albums = useAlbumsStore((state) => state.albums)
   const { query, order, onlyBest } = useFilterSettingsContext()
   const sorted = filterAlbums(albums, query, order, onlyBest)
   const { view } = useViewContext()
+
+  if (!albums.length)
+    return (
+      <h3 className="text-center text-xl mt-8">
+        {internationalizeMessage({
+          en: "No albums yet 😜",
+          pl: "Nie dodano jeszcze albumów 😜",
+        })}
+      </h3>
+    )
 
   return view === "grid" ? <Grid albums={sorted} /> : <Table albums={sorted} />
 }
